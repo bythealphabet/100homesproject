6846448ced359471e47f2fab621305201a1f2d71
Isaac lucas <isaaclucas@pop-os.localdomain>
2024-12-23 19:19:38 -0400

    wow

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from 'next';
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  basePath: isProduction ? '/100homesproject' : undefined,
+  // basePath: isProduction ? '/100homesproject' : undefined,
+  basePath: '/100homesproject',
   images: {
     domains: ['www.balitecture.com'],
   },
